724674e7695dc1c60901d5ba97f038f5e3e08068
crocket77 <ethanpfeiffer345@outlook.com>
2022-02-19 11:17:20 -0600

    added question object array, Improved on timer quiz functionality, gave an end screen for the end of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,26 @@ var startBtn = document.getElementById('start');
 var startScreen = document.getElementById('start-screen');
 //question screen
 var questionScreen= document.getElementById('questions')
+//high score link
+var highScoreLink= document.getElementById('hslink')
 
+// QUESTIONS SCREEN
+// reference questions screen
+const questionsScreen = document.getElementById('questions');
+// reference location where questions will display
+const questionText = document.getElementById('question-title');
+// reference locations where answer choices will display
+const answer1Text = document.getElementById('answer1');
+const answer2Text = document.getElementById('answer2');
+const answer3Text = document.getElementById('answer3');
+const answer4Text = document.getElementById('answer4');
 
 //timer display
 let timerEl = document.getElementById('qTime');
+var questionIndex=0;
 
-
+const endScreen = document.getElementById('end-screen');
+let displayedScore = document.getElementById('userTime');
 
 
 let timeCount;
@@ -28,12 +42,16 @@ function startQuiz() {
 
     // display questions screen
     questionScreen.classList.remove('hide');
-
+    
+    displayQuestions();
 
 };
 
 // start timer 
 function startTimer() {
+    //hide highscore link
+    highScoreLink.classList.add('hide');
+
     // setInterval @ 1sec
     timeInterval = setInterval(function() {
         // deduct timerCount
@@ -43,11 +61,149 @@ function startTimer() {
         if (timeCount <= 0) {
             // hide questions screen
             questionScreen.classList.add('hide');
+            endGame();
         }
     }, 1000);
 };
 
+// questions array
+const quizQuestions = [
+    {
+        question: 'Which language is used for styling?',
+        answer1: 'CSS',
+        answer2: 'HTML',
+        answer3: 'Javascript',
+        answer4: 'All of these',
+        answer: 'CSS'
+    },    {
+        question: 'Which language is used for logic?',
+        answer1: 'CSS',
+        answer2: 'HTML',
+        answer3: 'Javascript',
+        answer4: 'All of these',
+        answer: 'Javascript'
+    },    {
+        question: 'Which language is used for Structure of the page?',
+        answer1: 'CSS',
+        answer2: 'HTML',
+        answer3: 'Javascript',
+        answer4: 'All of these',
+        answer: 'HTML'
+    },    {
+        question: 'Which language is used for making webpages?',
+        answer1: 'CSS',
+        answer2: 'HTML',
+        answer3: 'Javascript',
+        answer4: 'All of these',
+        answer: 'All of these'
+    },    {
+        question: 'Which language is uses for loops?',
+        answer1: 'CSS',
+        answer2: 'HTML',
+        answer3: 'Javascript',
+        answer4: 'All of these',
+        answer: 'Javascript'
+    },    {
+        question: 'Which language is used for processing data?',
+        answer1: 'CSS',
+        answer2: 'HTML',
+        answer3: 'Javascript',
+        answer4: 'All of these',
+        answer: 'Javascript'
+    },    
 
+];
 
+// display questions
+function displayQuestions() {
+
+    // set question of current index
+    let currQuestion = quizQuestions[questionIndex];
+
+    if(questionIndex!=quizQuestions.length){
+    // display current question 
+    questionText.innerText = currQuestion.question;
+    }
+    else{
+        endGame();
+    }
+
+};
+
+// get value from clicked button and compare to correct answer
+function clickedAnswer(event) {
+
+    // let userAnswer = the text of the button the user clicked
+    let userAnswer = event.target.innerText;
+    
+    // make button clicked out of focus to return 
+    event.target.blur();
+
+    // if userAnswer = the answer to the current question show next question
+    if (userAnswer === quizQuestions[questionIndex].answer) {
+
+        // if the current question is the last question, end the game
+        if (questionIndex === 5) {
+            endGame();
+        }
+        // else, go to the next question
+        else {
+            questionIndex+=1;
+            displayQuestions();
+        }
+    } 
+    // if the question is incorrect, subtract 10 secs and move onto next question or end game
+    else {
+
+        // deduct by 10sec
+        timeCount-=2;
+
+        // turn button clicked with wrong answer red
+        event.target.classList.remove('btn-info');
+        event.target.classList.add('btn-danger');
+
+        if (questionIndex === 5 && userAnswer != quizQuestions[questionIndex].answer) {
+            // delay display of end screen
+            setTimeout(function () {
+                // turn button clicked back to blue
+                event.target.classList.remove('btn-danger');
+                event.target.classList.add('btn-info');
+                
+                // end the game
+                endGame();
+            }, 1000);
+
+        }
+        else {
+            // delay display of next question 
+            setTimeout(function () {
+                // turn button clicked back to blue
+                event.target.classList.remove('btn-danger');
+                event.target.classList.add('btn-info');
+
+                // go to next question
+                questionIndex++;
+                displayQuestions();
+            }, 1000);
+        }
+    }
+};
+function endGame() {
+    let userScore = timeCount;
+    // stop the time
+    clearInterval(timeInterval);
+
+    // hide questions screen
+    questionsScreen.classList.remove('start');
+    questionsScreen.classList.add('hide');
+
+    // end game screen
+    endScreen.classList.remove('hide');
+    endScreen.classList.add('start');
+
+    // // grab the time left as a score and display it on the screen
+    // let displayedScore = document.getElementById('final-score');
+    displayedScore.innerText = userScore;
+};
 
 startBtn.addEventListener('click', startQuiz);
